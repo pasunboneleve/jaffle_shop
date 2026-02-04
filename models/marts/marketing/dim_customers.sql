ebdfed4297f7cdc842229b889bd6e7aac2836bf7
Daniel Vianna <dmlvianna@gmail.com>
2026-02-04 03:30:54 +0000

    dim_customers with successful and completed orders

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -7,6 +7,14 @@ with customers as (
 orders as (
 
     select * from {{ ref('stg_jaffle_shop__orders') }}
+    where status = 'completed'
+
+), 
+
+payments as (
+
+    select * from {{ ref('stg_stripe__payments') }}
+    where status = 'success'
 
 ),
 
@@ -17,9 +25,11 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
 
     from orders
+    left join payments using (order_id)
 
     group by 1
 
@@ -34,7 +44,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
 
     from customers
 
