a784ef53588f3e22587887b5f84bd52f42217949
Daniel Vianna <dmlvianna@gmail.com>
2026-02-04 03:40:09 +0000

    refactor dim_customers

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -10,16 +10,23 @@ payments as (
 
 ),
 
+order_payments as (
+
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+    from payments
+    group by 1
+
+),
+
 final as (
     select
     orders.order_id,
     orders.customer_id,
-    sum(payments.amount) as amount
+    coalesce(order_payments.amount, 0) as amount
     from orders
-    left join payments using (order_id)
-    where orders.status = 'completed'
-    and payments.status = 'success'
-    group by 1, 2
+    left join order_payments using (order_id)
 )
 
 select * from final
