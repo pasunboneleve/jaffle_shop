ad75e73d5cdcbb78137aa428ba8abc9366c62dc3
Daniel Vianna <dmlvianna@gmail.com>
2026-02-11 02:56:44 +0000

    renames and conversions in stripe

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -9,12 +9,12 @@ source as (
 renamed as (
 
     select
-        id,
-        orderid,
-        paymentmethod,
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
         status,
-        amount,
-        created,
+        amount / 100 as amount,
+        created as created_at,
         _batched_at
 
     from source
